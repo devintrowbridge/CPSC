4a4086384b6e61e93bfc98707f826fb0d4142bf0
Devin Trowbridge <devin.trow@gmail.com>
2020-05-26 21:31:25 -0500

    cpsc1213: Add activity 01

---
+++ cpsc1213/Activity_01/StudentInfo.java
@@ -0,0 +1,22 @@
+/**
+ * Simple program to print student info
+ *
+ * Activity 1
+ * @author Devin Trowbridge CPSC-1213-AO1
+ * @version 2020-05-26
+ */
+
+public class StudentInfo
+{
+   /**
+    * Prints information about a student.
+    * @param args Command line arguments - not used
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Devin Trowbridge");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   COMP1200 - Introduction to Computing\n"
+                       + "   UAH's C++ Developer Certification");
+   }
+}
