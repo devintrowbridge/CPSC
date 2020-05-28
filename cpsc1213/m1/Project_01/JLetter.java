2e78fb868e9053702020ed2533135345736fcc4a
Devin Trowbridge <devin.trow@gmail.com>
2020-05-27 20:45:44 -0500

    cpsc1213: Add project 01

---
+++ cpsc1213/m1/Project_01/JLetter.java
@@ -0,0 +1,28 @@
+/**
+ * Prints a large letter J using the characters in the word "JAVA".
+ *
+ * Project 1
+ * @author Devin Trowbridge CPSC-1213-AO1
+ * @version 2020-05-27
+ */
+
+public class JLetter
+{
+   /**
+    * Prints the letter J using chars in "JAVA".
+    * @param args Command line arguments - not used
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA\n"
+                       + "JAVAJAVAJAVA\n"
+                       + "      JAVA  \n"
+                       + "      JAVA  \n"
+                       + "      JAVA  \n"
+                       + "      JAVA  \n"
+                       + "J     JAVA  \n"
+                       + "JA    JAVA  \n"
+                       + " JAVAJAVA   \n"
+                       + "  JAVAJA    ");
+   }
+}
---
+++ cpsc1213/m1/Project_01/MyLifeGoals.java
@@ -0,0 +1,37 @@
+/**
+ * Simple program to student life goals.
+ *
+ * Project 1
+ * @author Devin Trowbridge CPSC-1213-AO1
+ * @version 2020-05-27
+ */
+
+public class MyLifeGoals
+{
+   /**
+    * Prints life goals of a student.
+    * @param args Command line arguments - not used
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Devin Trowbridge\n");
+      
+      // Short term goals
+      System.out.println(
+           "My short term goals are to earn a Bachelor of CS degree from "
+         + "Auburn University in the next two years. I also want to go to "
+         + "Jumpmaster school.");
+         
+      // Medium term goals
+      System.out.println(
+           "My medium term goals are to get a job outside of the defense "
+         + "industry where I currently work. I would rather work on fighting "
+         + "climate change or environmental protection.");
+         
+      // Long term goals
+      System.out.println(
+           "My long term goals are to live in a permaculture hobbit hole "
+         + "surrounded by the forest and computers with my wife, our two "
+         + "children, dogs, cats, chickens, horses, etc.");
+   }
+}
