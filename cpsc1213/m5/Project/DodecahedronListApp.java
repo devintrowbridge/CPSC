d7fca79324cdb1893dfddf97f87abe0790f5c9ce
Devin Trowbridge <devin.trow@gmail.com>
2020-06-23 21:39:34 -0500

    cpsc1213: fixup module 5 homework

--- cpsc1213/m5/Project/DodecahedronListApp.java
+++ cpsc1213/m5/Project/DodecahedronListApp.java
@@ -18,8 +18,10 @@ public class DodecahedronListApp {
     * report about the scanned data.
     *
     * @param args Command line arguments - not used
+    * @throws FileNotFoundException if passed file name can't be found
     */
    public static void main(String[] args) throws FileNotFoundException {
+     // Initialize variables
       Scanner cliScan, fileScan;
       ArrayList<Dodecahedron> dodecahedronArrayList 
          = new ArrayList<Dodecahedron>();
@@ -27,12 +29,16 @@ public class DodecahedronListApp {
       String label, color;
       double edge;
    
+      // Get file name from the command line interface
       cliScan = new Scanner(System.in);
       System.out.print("Enter file name: ");
       fileScan = new Scanner(new File(cliScan.nextLine()));
       
       listName = fileScan.nextLine();
       
+      // While there is still data in the file, keep grabbing lines
+      // WARNING: Undefined behavior when the number of lines after the list 
+      // name is not divisible by 3.
       while (fileScan.hasNext()) {
          label = fileScan.nextLine();
          color = fileScan.nextLine();
@@ -40,10 +46,11 @@ public class DodecahedronListApp {
          dodecahedronArrayList.add(new Dodecahedron(label, color, edge));
       }
       
+      // Stuff it into the dodecahedron list class
       DodecahedronList dodecahedronList 
          = new DodecahedronList(listName, dodecahedronArrayList);
       
-      
+      // Print output
       System.out.print("\n" + dodecahedronList);
       System.out.print("\n\n" + dodecahedronList.summaryInfo());
    }
