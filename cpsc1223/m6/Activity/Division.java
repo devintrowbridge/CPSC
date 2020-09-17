344bfb3acdf62814a5e25601eae965411e955ab9
Devin Trowbridge <devin.trow@gmail.com>
2020-09-16 21:48:01 -0500

    cpsc1223: finish activity 6

---
+++ cpsc1223/m6/Activity/Division.java
@@ -0,0 +1,43 @@
+/**
+* Provides static methods for dividing numbers.
+*
+* @author Devin Trowbridge CPSC-1223-AO1
+* @version 2020-09-20
+*/
+public class Division {
+
+   /**
+    * Returns the integer value of a division operation.
+    *
+    * @param numerator top number
+    * @param denominator bottom number
+    * @return integer result
+    */
+   public static int intDivide(int numerator, int denominator) {
+      try
+      {
+         return numerator / denominator;
+      }
+      catch (ArithmeticException e)
+      {
+         return 0;
+      }
+   }
+   
+   /**
+    * Returns the floating point value of a division operation.
+    *
+    * @param numerator top number
+    * @param denominator bottom number
+    * @return floating point result
+    * @throws IllegalArgumentException
+    */
+   public static double decimalDivide(int numerator, int denominator) 
+      throws IllegalArgumentException {
+      if (denominator == 0) {
+         throw new IllegalArgumentException("The denominator " 
+                                            + "cannot be zero");
+      }
+      return ((double) numerator / denominator);
+   }
+}
