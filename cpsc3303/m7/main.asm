e8f05a5df3d53545be0f22ce857c8e68d0384fc9
Devin Trowbridge <devin.trow@gmail.com>
2020-10-01 22:07:42 -0500

    cpsc3303: finish code for module 7

--- cpsc3303/m7/main.asm
+++ cpsc3303/m7/main.asm
@@ -26,7 +26,7 @@ ExitProcess proto,dwExitCode:dword
   positionPrompt   BYTE "Please enter the position P where to insert: ",0
 .code
 main PROC
-  ;call  Exercise1
+  call  Exercise1
   call  WriteCRLF
   call  WriteCRLF
   
@@ -95,39 +95,54 @@ Exercise1 ENDP
 ; --- none
 ; Parameter Registers
 ; --- edi: offset of the string
-; --- bl: number of characters to delete
-; --- dl: position to delete from 
-; --- cl: the length of the string 
+; --- bl:  number of characters to delete
+; --- dl:  position to delete from 
+; --- cl:  the length of the string 
 ; Intermediary Registers
 ; --- esi: points at the substring just past the characters to be deleted
 ; --- al:  contains character to move 
 ; --- ebx: holds the number of characters to delete
 ; --- edx: holds the position to delete from
+; --- ecx: holds the length of the string
 ; --------------------------------------------
 DeleteFromString PROC
-  push  edi       ; Save intermediary registers
+  push  esi       ; Save intermediary registers
   push  ax
   push  edx
   push  ebx
+  push  ecx
   
   and   ebx,0FFh
   and   edx,0FFh
+  and   ecx,0FFh
   
   add   edi,edx   ; move the string pointer to the delete position
   mov   esi,edi   ; move pointer to the end of the section that will be deleted
   add   esi,ebx   
-  sub   ecx,ebx   ; subtract deleted characters from loop counter
   
   cld             ; Copy string after deleted characters to delete position
   rep   movsb
   
+  pop   ecx
   pop   ebx
   pop   edx
   pop   ax
-  pop   edi
+  pop   esi
   ret
 DeleteFromString ENDP
 
+; --------------------------------------------
+; Result Registers
+; --- none
+; Parameter Registers
+; --- none
+; Intermediary Registers
+; --- edx: holds string offsets 
+; --- ecx: holds size of strings 
+; --- al:  receives result from readHex
+; --- edi: holds offset of string to delete chars from
+; --- ebx: holds number of characters to delete
+; --------------------------------------------
 Exercise2 PROC
   push edx                           ; 0) Push used registers onto the stack
   push ecx
@@ -180,11 +195,13 @@ Exercise2 ENDP
 ; Parameter Registers
 ; --- edi: offset of string S_1
 ; --- ebx: offset of string S_2
-; --- cl: the size of string S_1
-; --- al: the size of string S_2
-; --- dl: position P
+; --- cl:  the size of string S_1
+; --- al:  the size of string S_2
+; --- dl:  position P
 ; Intermediary Registers
-; --- none
+; --- ecx: holds size of string s_1
+; --- eax: holds size of string s_2
+; --- edx: holds the position to insert at
 ; --------------------------------------------
 InsertString PROC
   push  ecx
@@ -195,21 +212,27 @@ InsertString PROC
   and   eax,0FFh
   and   edx,0FFh
 
-  dec eax        ; Decrement eax to ignore the null character at the end
+  ; This first loop makes room for S_2 by shifting S_1 to the right 
+  ; by EAX number of characters. The loop stops once it gets to where
+  ; the end of S_2 will be.
+  add   edi,ecx   ; point edi at the end of S_1
+  mov   esi,edi   ; point esi at the end of S_1
+  add   edi,eax   ; move edi eax characters past the end of S_1
 
-  add   edi,ecx  ; Move the S_1 pointer to the end of the string
-  mov   esi,edi  ; Put esi at the last character of the string
-  add   edi,eax  ; Move the S_1 pointer P past the end of the string
-  add   edi,edx
-
+  ; Only copy what we need so edi will be in the right place to start 
+  ; copying S_2
+  sub   ecx,edx   
   std           
-  rep   movsb    ; Shift the entire string P places to the right
+  rep   movsb    
   
+  ; Now that we shifted S_1, have room for S_2, and EDI is at the address 
+  ; where the last character of S_2 will be, we start copying S_2 into the S_1 
+  ; buffer from right to left.
   mov   esi,ebx  ; Point esi at string S_2
-  add   esi,eax  ; Point esi at the end of string S_2
+  add   esi,eax  ; point esi at the end of S_2
+  dec   esi      ; don't grab the null termination character
 
   mov   ecx,eax  ; loop for length of string S_2
-  inc   ecx      ; incrememnt ecx so we don't leave off the first character in the string
   std           
   rep   movsb    ; write the S_2 string backwards
   
