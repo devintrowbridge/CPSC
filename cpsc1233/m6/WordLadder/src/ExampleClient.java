37df646c9ef1a0b26f5e30fea11b8bc274522aa7
Devin Trowbridge <devin.trow@gmail.com>
2020-11-26 19:35:10 -0600

    cpsc4733: intro to open source ethics paper

--- cpsc1233/m6/WordLadder/src/ExampleClient.java
+++ cpsc1233/m6/WordLadder/src/ExampleClient.java
@@ -23,23 +23,7 @@ public class ExampleClient {
     public static void main(String[] args) throws FileNotFoundException {
         WordLadderGame doublets = new Doublets(new FileInputStream(new File("/home/devin/workspace/CPSC/cpsc1233/m6/WordLadder/src/wordlists/sowpods.txt")));
 
-        System.out.println(doublets.getHammingDistance("tiger", "tiger"));
-        System.out.println(doublets.getHammingDistance("tiger", "eagle"));
-        System.out.println(doublets.getHammingDistance("war", "eagle"));
-        System.out.println(doublets.getHammingDistance("barner", "bammer"));
-
-        System.out.println(doublets.isWord("tiger"));
-        System.out.println(doublets.isWord("eagle"));
-        System.out.println(doublets.isWord("aubie"));
-
-        System.out.println(doublets.getWordCount());
-
-        System.out.println(doublets.isWordLadder(Arrays.asList("cat", "cot", "zot", "dot")));
-        System.out.println(doublets.isWordLadder(Arrays.asList("cat", "cot", "pot", "dot")));
-
-        System.out.println(doublets.getNeighbors("tiger"));
-
-        System.out.println(doublets.getMinLadder("cat", "dog"));
+        System.out.println(doublets.getMinLadder("tiger", "eagle"));
     }
 }
 
