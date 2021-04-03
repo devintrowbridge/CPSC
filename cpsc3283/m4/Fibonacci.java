8248067da737752bafa7eddeb967911c9fb10e88
ncporter <ncporter@umich.edu>
2021-04-02 21:17:26 -0400

    CPSC3283: cleaned up

--- cpsc3283/m4/Fibonacci.java
+++ cpsc3283/m4/Fibonacci.java
@@ -12,7 +12,7 @@ public static void collectData() throws IOException {
     FileWriter out = new FileWriter("output.txt");
     
     out.write(",Recursive,,Dynamic,,Iterative\n");
-    //out.write("");
+    
     long startTime = 0;
     long endTime = 0;
     String outString = "";
@@ -79,23 +79,11 @@ public static int dpFibonacci(int n)    {
         table[i] = table[i-1] + table[i-2];
     }
     return table[n];
-
-    //int f = dpFibonacci(n-1) + dpFibonacci(n-2);
-    //table[n] = f;
-   // return f;     
+     
     }
 
     public static int iterativeFibonacci(int n) {
-        //if (n <=2)  {
-        //    return 1;
-       // }
-       // int[] array = new int[n];
-       // array[0] = 1;
-       // array[1] = 1;
-       // for (int i = 2; i < array.length; i++)  {
-      //      array[i] = array[i-2] + array[i-1];
-       // }
-       // return array[n-1];
+       
 
         int fib = 0;
         int x = 1;
